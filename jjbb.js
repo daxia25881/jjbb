4839de19c0d888ac4399cf5381ce9875e892d61c
daxia25881 <112150335+daxia25881@users.noreply.github.com>
2025-12-12 17:36:06 +0800

    Update jjbb.js

--- jjbb.js
+++ jjbb.js
@@ -1,6 +1,6 @@
 [Script]
 # 匹配 bav53.cc 和 avjb.com 的视频详情页
-http-response ^https?:\/\/(www\.)?(bav53\.cc|avjb\.com)\/videos\/\d+ script-path=https://github.com/daxia25881/jjbb/blob/main/aiwei_crack.js, requires-body=true, timeout=10, tag=艾薇解析
+http-response ^https?:\/\/(www\.)?(bav53\.cc|avjb\.com)\/videos\/\d+ script-path=https://raw.githubusercontent.com/daxia25881/jjbb/refs/heads/main/aiwei_crack.js, requires-body=true, timeout=10, tag=艾薇解析
 
 [MITM]
 hostname = bav53.cc, avjb.com
